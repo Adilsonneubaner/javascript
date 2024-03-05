0e885d3f5cd77e340145bd02ab9e21cb7bf1a759
Adilson Neubaner <151559307+Adilsonneubaner@users.noreply.github.com>
2024-03-04 21:41:19 -0300

    criei aula06\ex006

---
+++ exercicios/aula05/ex001.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`Você estava à ${vel} Km/h`)
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade, MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança')
