198901e0086f4ae79a5386283b46a4e09fc60402
Adilson Neubaner <151559307+Adilsonneubaner@users.noreply.github.com>
2024-03-05 19:12:07 -0300

    atualização

--- exercicios/aula07/ex01/script/script.js
+++ exercicios/aula07/ex01/script/script.js
@@ -1,18 +1,21 @@
-var agora = new Date()
-var hora = agora.getHours()
-var horario = window.document.getElementById('horario')
-var fundo = window.document.getElementsByTagName('body')[0]
-var imagem = window.document.getElementById('imagem_circular')
-if(hora < 4){
+function carregar(){
+    var agora = new Date()
+    var hora = agora.getHours()
+    var horario = window.document.getElementById('horario')
+    var fundo = window.document.getElementsByTagName('body')[0]
+    var img = window.document.getElementById('foto')
     horario.innerHTML = `<p>Agora são ${hora} horas.</p>`
-    fundo.style.backgroundColor = '#120A0D'
-} else if(hora >= 4 && hora < 12){
-    horario.innerHTML = `<p>Agora são ${hora} horas.</p>`
-    fundo.style.backgroundColor = '#F3B067'
-} else if(hora >= 12 && hora <= 18){
-    horario.innerHTML = `<p>Agora são ${hora} horas.</p>`
-    fundo.style.backgroundColor = '#9B431F'
-} else if(hora > 18){
-    horario.innerHTML = `<p>Agora são ${hora} horas.</p>`
-    fundo.style.backgroundColor= '#241D39'
+    if(hora < 4){
+        fundo.style.backgroundColor = '#120A0D'
+        img.src = 'madrugada_circulo.png'
+    } else if(hora >= 4 && hora < 12){
+        fundo.style.backgroundColor = '#F3B067'
+        img.src = 'manha_circulo.png'
+    } else if(hora >= 12 && hora <= 18){
+        fundo.style.backgroundColor = '#9B431F'
+        img.src = 'tarde_circulo.png'
+    } else if(hora > 18){
+        fundo.style.backgroundColor= '#241D39'
+        img.src = 'noite_circulo.png'
+    }
 }
